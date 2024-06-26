3b8dd9502e4897dea1894d4c2d4aa8125fae477c
seablood <dudwnszero99@gmail.com>
2024-06-26 18:34:56 +0900

    study 내용 추가

--- chapter3/express-server/hello-express.js
+++ chapter3/express-server/hello-express.js
@@ -1,14 +1,14 @@
-const express = require("express"); // ❶
-const app = express(); // ❷
+const express = require("express"); // ❶ express 모듈 로딩
+const app = express(); // ❷ express 초기화 및 할당
 const port = 3000;
 
 // prettier-ignore
-app.get("/", (req, res) => { // ❸ 
-  res.set({ "Content-Type": "text/html; charset=utf-8" }); // ➍
+app.get("/", (req, res) => { // ❸ "/"에 매핑된 get 메소드 지정
+  res.set({ "Content-Type": "text/html; charset=utf-8" }); // ➍ 헤더 설정
   res.end("헬로 Express");
 });
 
 // prettier-ignore
-app.listen(port, () => { // ➎
+app.listen(port, () => { // ➎ 
   console.log(`START SERVER : use ${port}`);
 });
