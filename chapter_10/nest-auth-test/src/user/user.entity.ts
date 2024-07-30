291542c2a38348c8f63435074ba9ba54fcfe8e9d
seablood <dudwnszero99@gmail.com>
2024-07-30 12:05:10 +0900

    study 내용 추가

---
+++ chapter_10/nest-auth-test/src/user/user.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({unique: true})
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    username: string;
+
+    @Column({type: 'datetime', default: () => 'CURRENT_TIMESTAMP'})
+    createdDate: Date = new Date();
+
+}
